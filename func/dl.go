f49f5bcbc09218c4c451c89a8b6985f6e2f27488
SevralT <a.chycherin@outlook.com>
2022-05-28 23:50:31 +0300

    feat: Add realization of (non-)progressbar download modes * Also add multi-lang support

--- func/dl.go
+++ func/dl.go
@@ -1,59 +1,37 @@
 package dl
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"strings"
-	"github.com/dustin/go-humanize"
+
+	"github.com/schollz/progressbar/v3"
 )
 
-var FileName, FileUrl string
+var FileName, FileUrl, File_download string
+var Progress bool
 
 func DownloadFile(url string, filepath string) error {
-	out, err := os.Create(filepath + ".godl")
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	resp, err := http.Get(FileUrl)
-	if err != nil {
-		return err
-	}
+	resp, _ := http.Get(FileUrl)
 	defer resp.Body.Close()
 
-	count := &WriteCounter{}
-	_, err = io.Copy(out, io.TeeReader(resp.Body, count))
-	if err != nil {
-		return err
+	temp, _ := os.Create(filepath + ".godl")
+	defer temp.Close()
+
+	if Progress == false {
+		total := progressbar.DefaultBytes(
+			-1,
+			File_download,
+		)
+		io.Copy(io.MultiWriter(temp, total), resp.Body)
+	} else if Progress == true {
+		bar := progressbar.DefaultBytes(
+			resp.ContentLength,
+			File_download,
+		)
+		io.Copy(io.MultiWriter(temp, bar), resp.Body)
 	}
 
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	err = os.Rename(FileName+".godl", FileName)
-	if err != nil {
-		return err
-	}
+	os.Rename(FileName+".godl", FileName)
 	return nil
-}
-
-type WriteCounter struct {
-	Total uint64
-}
-
-func (wc *WriteCounter) Write(p []byte) (int, error) {
-	n := len(p)
-	wc.Total += uint64(n)
-	wc.PrintProgress()
-	return n, nil
-}
-
-func (wc WriteCounter) PrintProgress() {
-	fmt.Printf("\r%s", strings.Repeat(" ", 50))
-	fmt.Printf("\rЗавершено %s", humanize.Bytes(wc.Total))
 }
--- main.go
+++ main.go
@@ -8,27 +8,40 @@ import (
 	"path"
 
 	dl "github.com/SevralT/GoDL/func"
+	"github.com/jeandeaual/go-locale"
 )
 
 func main() {
+	userLanguage, _ := locale.GetLanguage()
+
+	var usage, finished string
+	if userLanguage == "ru" {
+		usage = "Использование: godl [-u url] [-n filename] [-p]"
+		dl.File_download = "Загрузка файла"
+		finished = "окончена!"
+	} else if userLanguage == "uk" {
+		usage = "Використання: godl [-u url] [-n filename] [-p]"
+		dl.File_download = "Завантаження файлу"
+		finished = "закінчено!"
+	} else {
+		usage = "Usage: godl [-u url] [-n filename] [-p]"
+		dl.File_download = "Downloading file"
+		finished = "finished!"
+	}
+
 	if len(os.Args) < 2 {
-		fmt.Println("Использование: godl [-u url] [-n filename]")
+		fmt.Println(usage)
 		os.Exit(0)
 	}
 
 	var filename string
 	flag.StringVar(&dl.FileUrl, "u", "nil", "Указать URL-адрес")
 	flag.StringVar(&filename, "n", "nil", "Указать пользовательское название файла")
+	flag.BoolVar(&dl.Progress, "p", false, "Использовать прогресс-бар")
 	flag.Parse()
 
-	if dl.FileUrl == "nil" && filename == "nil" {
-		dl.FileUrl = os.Args[1]
-		r, _ := http.NewRequest("GET", dl.FileUrl, nil)
-		dl.FileName = path.Base(r.URL.Path)
-	}
-
 	if dl.FileUrl == "nil" {
-		fmt.Println("Использование: godl [-u url] [-n filename]")
+		fmt.Println(usage)
 		os.Exit(0)
 	}
 
@@ -39,11 +52,6 @@ func main() {
 		dl.FileName = filename
 	}
 
-	fmt.Print("Загрузка файла ", dl.FileName, "...")
-
-	err := dl.DownloadFile(dl.FileUrl, dl.FileName)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("\rЗагрузка файла", dl.FileName, "окончена!")
+	dl.DownloadFile(dl.FileUrl, dl.FileName)
+	fmt.Println(dl.File_download, dl.FileName, finished)
 }

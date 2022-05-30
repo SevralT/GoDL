311d0d24990ad685c509d7eeb1294fba592f625d
SevralT <a.chycherin@outlook.com>
2022-05-30 22:51:06 +0300

    chore: Add comments to make code more readable

--- func/dl.go
+++ func/dl.go
@@ -1,5 +1,6 @@
-package dl
+package logic
 
+// Import required packages
 import (
 	"fmt"
 	"io"
@@ -10,16 +11,21 @@ import (
 	"github.com/schollz/progressbar/v3"
 )
 
+// Set global variables
 var FileName, FileUrl, File_download string
 var Progress bool
 
+// Function for file download
 func DownloadFile(url string, filepath string) error {
+	// Download file
 	resp, _ := http.Get(FileUrl)
 	defer resp.Body.Close()
 
+	// Create temp file for download
 	temp, _ := os.Create(filepath + ".godl")
 	defer temp.Close()
 
+	// Use progress bar or no
 	if Progress == false {
 		total := progressbar.DefaultBytes(
 			-1,
@@ -34,11 +40,14 @@ func DownloadFile(url string, filepath string) error {
 		io.Copy(io.MultiWriter(temp, bar), resp.Body)
 	}
 
+	// Rename temp file
 	os.Rename(FileName+".godl", FileName)
 	return nil
 }
 
+// Function for check internet connections
 func Connected() (ok bool) {
+	// "Download file"
 	_, err := http.Get(FileUrl)
 	if err != nil {
 		return false
@@ -46,13 +55,17 @@ func Connected() (ok bool) {
 	return true
 }
 
+// Function for stdout
 func Stdout(url string) error {
+	// "Download" file
 	resp, _ := http.Get(FileUrl)
 	defer resp.Body.Close()
 
+	// Convert response in string
 	body, _ := ioutil.ReadAll(resp.Body)
 	bodyString := string(body)
 
+	// Output file content
 	fmt.Println(bodyString)
 	return nil
 }
--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+// Import required packages
 import (
 	"flag"
 	"fmt"
@@ -7,65 +8,73 @@ import (
 	"os"
 	"path"
 
-	dl "github.com/SevralT/GoDL/func"
+	logic "github.com/SevralT/GoDL/func"
 	"github.com/jeandeaual/go-locale"
 )
 
 func main() {
+	// Get OS language
 	userLanguage, _ := locale.GetLanguage()
 
+	// Locatization
 	var usage, finished, check_internet_connection, connected string
 	if userLanguage == "ru" {
 		usage = "Использование: godl [-n filename] [-p] [-stdout] URL..."
-		dl.File_download = "Загрузка файла"
+		logic.File_download = "Загрузка файла"
 		finished = "окончена!"
 		check_internet_connection = "Ошибка: Проверьте подключение к интернету!"
 		connected = "Соединение установлено!"
 	} else if userLanguage == "uk" {
 		usage = "Використання: godl [-n filename] [-p] [-stdout] URL..."
-		dl.File_download = "Завантаження файлу"
+		logic.File_download = "Завантаження файлу"
 		finished = "закінчено!"
 		check_internet_connection = "Помилка: Перевірте підключення до інтернету!"
 		connected = "З'єднання встановлено!"
 	} else {
 		usage = "Usage: godl [-n filename] [-p] [-stdout] URL..."
-		dl.File_download = "Downloading file"
+		logic.File_download = "Downloading file"
 		finished = "finished!"
 		check_internet_connection = "Error: Check internet connection!"
 		connected = "Connection established!"
 	}
 
+	// Check for args amount
 	if len(os.Args) < 2 {
 		fmt.Println(usage)
 		os.Exit(0)
 	}
 
+	// Set flags and args
 	var filename string
 	var stdout bool
 	flag.StringVar(&filename, "n", "", "Указать пользовательское название файла")
-	flag.BoolVar(&dl.Progress, "p", false, "Использовать прогресс-бар")
+	flag.BoolVar(&logic.Progress, "p", false, "Использовать прогресс-бар")
 	flag.BoolVar(&stdout, "stdout", false, "Вывести содержимое через stdout")
 	flag.Parse()
 
-	dl.FileUrl = flag.Arg(0)
+	logic.FileUrl = flag.Arg(0)
 
+	// Set filename
 	if filename == "" {
-		r, _ := http.NewRequest("GET", dl.FileUrl, nil)
-		dl.FileName = path.Base(r.URL.Path)
+		r, _ := http.NewRequest("GET", logic.FileUrl, nil)
+		logic.FileName = path.Base(r.URL.Path)
 	} else {
-		dl.FileName = filename
+		logic.FileName = filename
 	}
 
-	if !dl.Connected() {
+	// Check internet connection (ping url)
+	if !logic.Connected() {
 		fmt.Println(check_internet_connection)
 		os.Exit(0)
 	} else {
 		println(connected)
 	}
+
+	// Use stdout or download file
 	if stdout == true {
-		dl.Stdout(dl.FileUrl)
+		logic.Stdout(logic.FileUrl)
 	} else {
-		dl.DownloadFile(dl.FileUrl, dl.FileName)
-		fmt.Println(dl.File_download, dl.FileName, finished)
+		logic.DownloadFile(logic.FileUrl, logic.FileName)
+		fmt.Println(logic.File_download, logic.FileName, finished)
 	}
 }
